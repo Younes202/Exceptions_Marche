69efe2906e2820ea212cc0ec2904feb625694b81
Younes202 <84544672+Younes202@users.noreply.github.com>
2022-12-31 23:07:31 +0100

    Comments_up

--- Marche.java
+++ Marche.java
@@ -1,17 +1,18 @@
+// Realisé par :  Younes Sghyer
 import java.util.Scanner;
 
 class Main {
 
-  public static boolean si_nombre(String s) {
+  public static boolean si_nombre(String s) {  // Fonction qui vérifier est ce que la valeur entrée  nombre ou non 
     try {
-      Integer.parseInt(s);
-      return true;
-    } catch (NumberFormatException e) {
-      return false;
+        Integer.parseInt(s);
+        return true;
+    }catch (NumberFormatException e) {
+        return false;
     }
   }
 
-  public static int forcNumeric(String s) {
+  public static int forcNumeric(String s) {  // Fonction qui forcer un nombre de type string à un nombre de type integer
     return Integer.parseInt(s);
 
   }
@@ -24,20 +25,22 @@ class Main {
 
     Scanner input = new Scanner(System.in);
     int i, inpu;
-    String tt = "\t", inpu_str;
+    String tt = "\t", inpu_str;  
     boolean test = true;
-
+    // Pr tache : La vérification (Implémentation de la méthode isNumeric()) est ce que le nombre qui va entrer de type int ou non 
     do {
+     
       System.out.println("Enter a number ");
       inpu_str = input.nextLine();
-      test = si_nombre(inpu_str);
+      test = si_nombre(inpu_str); 
 
     } while (test != true);
-
-    inpu = forcNumeric(inpu_str);
-
+        // dm tache :  (Implémentation de la méthode forcNumeric()) pour rendre le nombre de type string à un entier 
+         inpu = forcNumeric(inpu_str);
+    
+        // 1 : si les valeurs qu'on a entrées est compatible avec les excigences qu'on a fait , les tests suivants seront exécutés [Félicitations : hhh]   
     for (i = inpu; i >= 1; i--) {
-      System.out.println(tt.repeat(i) + "_/");
+       System.out.println(tt.repeat(i) + "_/");
     }
 
   }
